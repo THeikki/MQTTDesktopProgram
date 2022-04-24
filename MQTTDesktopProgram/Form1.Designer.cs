3b57994c2f8e3b51b74d2f6671b5f7883e57c7f1
THeikki <t9tohe00@students.oamk.fi>
2022-04-24 20:07:32 +0300

    project started

---
+++ MQTTDesktopProgram/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace MQTTDesktopProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(246, 38);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(145, 21);
+            this.comboBox.TabIndex = 0;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(92, 38);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(92, 21);
+            this.connectButton.TabIndex = 1;
+            this.connectButton.Text = "Yhdistä";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Enabled = false;
+            this.textBox.Location = new System.Drawing.Point(92, 134);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(299, 104);
+            this.textBox.TabIndex = 2;
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(434, 317);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(75, 23);
+            this.quitButton.TabIndex = 4;
+            this.quitButton.Text = "Lopeta";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(92, 272);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Aloita";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 367);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.comboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ MQTTDesktopProgram/Form1.cs
@@ -0,0 +1,210 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MQTTDesktopProgram
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        DateTime time;
+        string value;
+        public float distance;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox.Items.Add(port);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort != null)
+            {
+                serialPort.Dispose();
+                serialPort.Close();
+            }
+            //isSaving = false;
+            Application.Exit();
+        }
+
+        /*
+         *  BUTTON ACTIONS   
+         * 
+        */
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            if (comboBox.Text != "")
+            {
+                try
+                {
+                    CreateSerialPortConnection();
+                    comboBox.Enabled = false;
+                    Thread t = new Thread(GetArduinoValue);
+                    t.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Virhe");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Valitse yhdistettävä portti ensin!", "Virhe");
+            }
+
+        }
+
+
+        private async void startButton_Click(object sender, EventArgs e)
+        {
+            await ConnectToBroker();
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null)
+            {
+                serialPort.Dispose();
+                serialPort.Close();
+            }
+
+            Application.Exit();
+        }
+
+        /*
+         *  FUNCTIONS
+         * 
+         */
+
+        public void CreateSerialPortConnection()    //Open serial connections
+        {
+            serialPort = new SerialPort();
+            serialPort.PortName = comboBox.SelectedItem.ToString();
+            serialPort.BaudRate = 9600;
+            serialPort.Parity = Parity.None;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Open();
+            connectButton.Text = "YHDISTETTY";
+            connectButton.BackColor = Color.LightGreen;
+            connectButton.Enabled = false;
+            comboBox.Enabled = false;
+        }
+
+        public void GetArduinoValue()    //  Get sensor values (temperature and gas) from Arduino
+        {
+            while (serialPort.IsOpen)
+            {
+                try
+                {
+                    value = serialPort.ReadLine();
+                    time = DateTime.Now;
+                    CheckIfValidValue();
+                    textBox.Text = "\r\n" + "Aika: " + time.ToString(("dd-MM-yyyy HH:mm:ss")) + "\r\n\r\n" + "Etäisyys: " + value.ToString();
+                    // Thread.Sleep(1000);
+                }
+                catch (Exception exe)
+                {
+                    MessageBox.Show(exe.Message, "Virhe");
+                }
+            }
+        }
+
+        public void CheckIfValidValue()
+        {
+            int num = 0;
+            char invalid = '.';
+
+            try
+            {
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (i == invalid)
+                    {
+                        num++;
+                        if (num == 2)
+                        {
+                            textBox.Text = "";
+                            return;
+                        }
+                    }
+                }
+
+                distance = (float)Convert.ToDouble(value);
+
+                if ( distance < 2 || distance > 400)
+                {
+                    textBox.Text = "";
+                    MessageBox.Show("Luku on mittausalueen ulkopuolella!", "Virhe");
+                }
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+        private static async Task ConnectToBroker()
+        {
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+
+            var messageBuilder = new MqttClientOptionsBuilder()
+                .WithClientId(Guid.NewGuid().ToString())
+                .WithTcpServer("test.mosquitto.org", 1883)
+                //.WithKeepAliveSendInterval((TimeSpan.FromSeconds(5))
+                .WithCleanSession()
+                .Build();
+
+            client.UseConnectedHandler(e =>
+            {
+                MessageBox.Show("Connected successfully with MQTT Brokers.");
+            });
+
+            client.UseDisconnectedHandler(e =>
+            {
+                MessageBox.Show("Disconnected from MQTT Brokers.");
+            });
+
+            await client.ConnectAsync(messageBuilder);
+
+            await PublishAsync(client);
+        }
+
+        private static async Task PublishAsync(IMqttClient client)
+        {
+            var mc = new Form1();
+            string payload = mc.value;
+
+            if (client.IsConnected)
+            {
+                await client.PublishAsync(new MqttApplicationMessageBuilder()
+                    .WithTopic("arduino/sensor/distance")
+                    .WithPayload(payload)
+                    .WithQualityOfServiceLevel(0)
+                    .Build());
+            }
+        }
+    }
+}
